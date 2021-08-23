a8ebf02da3b8e16c2754cb6bd40bab2a18346172
Mayank Umrao <73928358+MayankUmrao@users.noreply.github.com>
2021-08-23 23:42:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def predict():
         Kms_Driven2=np.log(Kms_Driven)
         Owner=int(request.form['Owner'])
         Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
+        if Fuel_Type_Petrol=='Petrol':
                 Fuel_Type_Petrol=1
                 Fuel_Type_Diesel=0
         elif Fuel_Type_Petrol=='Diesel':
@@ -39,7 +39,7 @@ def predict():
         else:
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=0
-        Year=2020-Year
+        Year=2021-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
             Seller_Type_Individual=1
